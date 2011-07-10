145d6e63976b3454c2ef2e52a3ac546986a71e36
Joe0 <joe.pritzel@gmail.com>
2011-07-10 07:18:55 -0500

    Added Squeryl library Added MySQL driver

--- Server/project/build/Project.scala
+++ Server/project/build/Project.scala
@@ -8,4 +8,7 @@ import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) {
 	val scalaTest = "org.scalatest" % "scalatest" % "1.4.RC2"
 	val netty = "org.jboss.netty" % "netty" % "3.2.4.Final"
+	val squeryl = "org.squeryl" %% "squeryl" % "0.9.4"
+
+	val mysqlDriver = "mysql" % "mysql-connector-java" % "5.1.10"
 }
