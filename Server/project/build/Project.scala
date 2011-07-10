b414af90485d62ee4eaca7612824f8c2fd011c10
Joe0 <joe.pritzel@gmail.com>
2011-07-10 07:13:42 -0500

    Updated .gitignore Added documentation to the build configuration.

--- Server/project/build/Project.scala
+++ Server/project/build/Project.scala
@@ -1,5 +1,10 @@
 import sbt._
 
+/**
+ * This is the build configuration.
+ *
+ * @author Joe Pritzel
+ */
 class Project(info: ProjectInfo) extends DefaultProject(info) {
 	val scalaTest = "org.scalatest" % "scalatest" % "1.4.RC2"
 	val netty = "org.jboss.netty" % "netty" % "3.2.4.Final"
