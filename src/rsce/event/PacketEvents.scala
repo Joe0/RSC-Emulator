09095060f6cd4413d8db16e5c5013b7156bd14d5
Joe0 <joe.pritzel@gmail.com>
2012-07-17 23:04:10 -0500

    Removed aliases in PacketEvents and created an object to define type in

--- src/rsce/event/PacketEvents.scala
+++ src/rsce/event/PacketEvents.scala
@@ -1,21 +1,27 @@
 package rsce.event
 
-import rsce.entity.{ Entity => E }
-import rsce.entity.traits.{ Networked => N }
+import rsce.entity.Entity
+import rsce.entity.traits.Networked
 import rsce.valueobject.Point
 
-case class DummyPacketEvent(entity : E with N, unknownByte : Byte, clientVersion : Short)
-case class TrapPacketEvent(entity : E with N)
-case class PingPacketEvent(entity : E with N, data : Byte)
-case class ReportPacketEvent(entity : E with N, reportedUsernameHash : Long, reason : Byte)
-case class InvUseOnPlayerPacketEvent(entity : E with N, otherPlayerHash : Long, slot : Short)
-case class InvUseOnItemPacketEvent(entity : E with N, slot1h : Short, slot2 : Short)
-case class InvUseOnGroundItemPacketEvent(entity : E with N, location : Point, groundItemId : Short, slot : Short)
-case class InvUseOnDoorPacketEvent(entity : E with N, location : Point, direction : Byte, slot : Short)
-case class CastOnGameObjectPacketEvent(entity : E with N, spellId : Int, point : Point)
-case class AddIgnorePacketEvent(entity : E with N, ignoreUsernameHash : Long)
-case class SessionRequestPacketEvent(entity : E with N, userByte : Byte, username : String)
-case class DeclineDuelPacketEvent(entity : E with N)
-case class LogoutPacketEvent(entity : E with N)
-case class SecondaryObjectAction(entity : E with N, location : Point)
-case class StyleChangePacketEvent(entity : E with N, style : Byte)
+private[event] object TypeDefs {
+  type E = Entity with Networked
+}
+
+import TypeDefs._
+
+case class DummyPacketEvent(entity : E, unknownByte : Byte, clientVersion : Short)
+case class TrapPacketEvent(entity : E)
+case class PingPacketEvent(entity : E, data : Byte)
+case class ReportPacketEvent(entity : E, reportedUsernameHash : Long, reason : Byte)
+case class InvUseOnPlayerPacketEvent(entity : E, otherPlayerHash : Long, slot : Short)
+case class InvUseOnItemPacketEvent(entity : E, slot1h : Short, slot2 : Short)
+case class InvUseOnGroundItemPacketEvent(entity : E, location : Point, groundItemId : Short, slot : Short)
+case class InvUseOnDoorPacketEvent(entity : E, location : Point, direction : Byte, slot : Short)
+case class CastOnGameObjectPacketEvent(entity : E, spellId : Int, point : Point)
+case class AddIgnorePacketEvent(entity : E, ignoreUsernameHash : Long)
+case class SessionRequestPacketEvent(entity : E, userByte : Byte, username : String)
+case class DeclineDuelPacketEvent(entity : E)
+case class LogoutPacketEvent(entity : E)
+case class SecondaryObjectAction(entity : E, location : Point)
+case class StyleChangePacketEvent(entity : E, style : Byte)
